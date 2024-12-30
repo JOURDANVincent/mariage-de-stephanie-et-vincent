dcb59cd2204be1ce560b1e5b93f3e2103028bc17
Vins <jourdan.v@orange.fr>
2024-12-30 19:00:47 +0100

    add title property to iframe

--- src/lib/components/Adresse.svelte
+++ src/lib/components/Adresse.svelte
@@ -15,13 +15,10 @@
     <div class="map">
         <div class="map-wrapper">
             <iframe 
+                title="localisation du Moulin de Saint Germain sur Bresles"
                 class=iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d586619.90267462!2d1.2358762953257598!3d49.83812498728498!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e74347ed98fec7%3A0x42fa0d7f8342946f!2sLe%20Moulin%20de%20Saint%20Germain%20sur%20Bresle!5e0!3m2!1sfr!2sfr!4v1735486656008!5m2!1sfr!2sfr" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"
                 width="100%" height="100%" 
             ></iframe>
-            <!-- <iframe 
-                class=iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2573.3455846704273!2d1.7242922126689306!3d49.83596377136255!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e74347ed98fec7%3A0x42fa0d7f8342946f!2sLe%20Moulin%20de%20Saint%20Germain%20sur%20Bresle!5e0!3m2!1sfr!2sfr!4v1735472291306!5m2!1sfr!2sfr" style="border:0;" allowfullscreen="true" loading="lazy" referrerpolicy="no-referrer-when-downgrade"
-                width="100%" height="100%"
-            ></iframe> -->
         </div>
     </div>
 
