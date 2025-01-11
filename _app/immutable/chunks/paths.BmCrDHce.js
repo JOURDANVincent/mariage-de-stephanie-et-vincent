d14f8f60737556cbae5d3fcac53f76601658ddd9
Vins <jourdan.v@orange.fr>
2025-01-11 15:04:22 +0100

    Updates

---
+++ _app/immutable/chunks/paths.BmCrDHce.js
@@ -0,0 +1,1 @@
+var e;const a=((e=globalThis.__sveltekit_14eg374)==null?void 0:e.base)??"/mariage-de-stephanie-et-vincent";var s;const t=((s=globalThis.__sveltekit_14eg374)==null?void 0:s.assets)??a;export{t as a,a as b};
